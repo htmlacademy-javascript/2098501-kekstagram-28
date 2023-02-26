7864023c4e3d14316471c365f503c6d8c15f1220
Andrei <derekheil042@gmail.com>
2023-02-26 15:56:40 +0300

    Пофиксил ошибки в линтере

--- js/functions.js
+++ js/functions.js
@@ -2,26 +2,32 @@
 
 const isLessOrEqual = (string, length) => string.length <= length;
 
+isLessOrEqual('проверяемая строка', 20);
+
 // Функция для проверки, является ли строка палиндромом
 
 const isPalindrom = (string) => {
-  const tempString = string.toLowerCase().replaceAll(" ", "");
+  const tempString = string.toLowerCase().replaceAll(' ', '');
 
-  return tempString === tempString.split("").reverse().join("");
+  return tempString === tempString.split('').reverse().join('');
 };
+
+isPalindrom('топот');
 
 // Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
 
 function getNum(param) {
-  const str = param.replaceAll(" ", "").split("");
-  let num = "";
+  const str = param.replaceAll(' ', '').split('');
+  let num = '';
   for (let i = 0; i <= str.length - 1; i++) {
-    if (+str[i] === parseInt(str[i])) {
+    if (+str[i] === parseInt(str[i], 10)) {
       num += +str[i];
     }
   }
   return Math.floor(num);
 }
+
+getNum('2023 год');
 
 // Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины
 
@@ -34,3 +40,5 @@ const myPadStart = (string, minLength, pad) => {
         pad.repeat(actualPad / pad.length) +
         string;
 };
+
+myPadStart('q', 4, 'werty');
